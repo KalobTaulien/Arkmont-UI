6fe8a411bed659e4e17932f6cc40c4bb36d0eab3
Kalob Taulien <kalob.taulien@gmail.com>
2017-01-30 20:23:11 -0700

    Moving to JS animations instead of CSS. CSS just can't quite do what we need it to do yet. :/

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,21 @@
 # Changelog
 This is to keep track of changes in a way that lets contributors *read* about what's going on, instead of looking through commits. 
 
+### Jan. 30, 2017
+- Decided to revert back to using JavaScript. It was a fun experiment, but CSS simply cannot handle animations the way we need it to. We'll try again in a couple years. 
+- Removed the option to set number of tiles per page. This might come back, but if it doesn't then we'll add auto-adjusting for smaller screens.
+- Added `mouseover` and `mouseout` jQuery events. This will be better for mobile users who don't have a mouse.
+- Changed tile padding (margin, technically) from 10px to 5px (2.5px on either side). It looks cleaner with less spacing between tiles.
+- There are no callbacks anymore. It's all even listeners. 
+- Removed window resizing function. We'll re-add that later when we work on the `tilesPerPage` option. 
+- Changed the way the `next` and `prev` tiles show their chevron arrows. There's now a child element called `.tile__hasnext` and `.tile__hasprev` that sits inside `.tile` to lay on top of the media and content elements. This solved the problem of each row always adjusting 1px after the images load.
+- `.gitignore` ignores a couple ESLinting files. We'll add linting in the near future.
+
+__Problems__:
+- Using jQuery to animate the tiles with CSS is causing tiles that have scaled to have slightly blurry text. It's not terrible, but definitely annoying. 
+- 
+
+
 ### Jan. 29, 2017
 - Changed the way the outer row (drop down section) slides down. It uses CSS3 Transition with the `max-height` attribute instead of the `height` attribute. 
 - `.tile__title` was changing sizes in different hover areas; it's now consistent. 
--- js/main.js
+++ js/main.js
@@ -9,23 +9,9 @@ $(document).ready(function () {
       // 5 is the default
       tilesPerPage: 5,
       // Default tile width with it's margin
-      tileWidth: 243,
+      tileWidth: 253,
       // Global variable for active tiles.
       aTileIsActive: false,
-      tileClickCallback: function () {}, // Function to run when a tile becomes "active"
-      tileDeactivatedCallback: function () {}, // Function to run when an active tile is no longer active
-      // Allow (when possible) localStorage caching instead of object based caching 
-      useLocalStorage: true,
-      // Check if the user can use localStoragae or not.
-      checkForLocalStorage: function() {
-        // We the user has the ability to use localStorage, AND the developer has this setting enabled. 
-        if(typeof Storage !== "undefined" && setup.settings.useLocalStorage) {
-          setup.settings.useLocalStorage = true;
-        } else {
-          // No localStorage support. Disable this option.
-          setup.settings.useLocalStorage = false;
-        }
-      },
       // _row is the row selector (jquery object)
       // currentPage is the current page number.
       setScrollButtons: function (_row, currentPage) {
@@ -38,8 +24,8 @@ $(document).ready(function () {
           var nextOverlayTile = ((currentPage + 1) * setup.settings.tilesPerPage);
           // If there is a previous "page" we need to apply the .tile--has-prev 
           // class to the last tile of that page. 
-          _row.find('.tile:eq(' + prevOverlayTile + ')').addClass('tile--has-prev');
-          _row.find('.tile:eq(' + (nextOverlayTile) + ')').addClass('tile--has-next')
+          _row.find('.tile:eq(' + prevOverlayTile + ')').addClass('tile--has-prev').prepend("<div class='tile__hasprev'></div>");
+          _row.find('.tile:eq(' + (nextOverlayTile) + ')').addClass("tile--has-next").prepend("<div class='tile__hasnext'></div>");
 
           // If this row is "active" we need to make sure the page that the user 
           // is sliding to will also have an active tile.
@@ -59,52 +45,18 @@ $(document).ready(function () {
     // Init the carousel
     init: function (args) {
 
-      // Force tilesPerPage with smaller devices
-      if( $(window).outerWidth(true) <= 450 ) {
-        // 2 tiles per page on smaller devices. 
-        this.settings.tilesPerPage = 2;
-      } else if( $(window).outerWidth(true) <= 768 ) {
-        // If the screen is tiny, make the tiles bigger (use less; max 3) 
-        this.settings.tilesPerPage = 3;
-      } else if (args.tilesPerPage !== undefined) {
-        // Set the tiles per page now.
-        // alert('set the tiles per page');
-        this.settings.tilesPerPage = args.tilesPerPage;
-      } else {
-        this.settings.tilesPerPage = this.settings.tilesPerPage; // default value
-      }
-
-      // If an argument is provided for the tileClickCallback, set it. 
-      if (args.tileClickCallback !== undefined) {
-        this.settings.tileClickCallback = args.tileClickCallback;
-      }
-
-      // If an argument is provided for the tileClickCallback, set it. 
-      if (args.tileDeactivatedCallback !== undefined) {
-        this.settings.tileDeactivatedCallback = args.tileDeactivatedCallback;
-      }
-
-      // If the useLocalStorage setting was given (as a boolean)
-      if( typeof(args.useLocalStorage) === "boolean" ) {
-        this.settings.useLocalStorage = args.useLocalStorage;
-      }
+      // Default tiles per page 
+      this.settings.tilesPerPage = 5;
 
       // Resize tiles when the viewport changes
       window.onresize = function (event) {
-          // Window resizing is a major problem. 
-          setup.init( setup.settings )
+          // Change the visible tiles. That's all. 
       }
-
-      // Set the localStorage setting. 
-      this.settings.checkForLocalStorage();
-
-      // The reason we do this is to give us accurate padding on both the left and right of
-      // each "page" so the prev and next buttons can exist
-
+      
       // Remove the 2 paddings on the left and right.
-      var width = $(window).outerWidth(true) - 40 - 40;
-      // 10px per tile. 
-      var totalTilePadding = this.settings.tilesPerPage * 10;
+      // 5px (2.5px on each side) per tile. 
+      var width = $(window).outerWidth(true) - 42.5 - 42.5;
+      var totalTilePadding = this.settings.tilesPerPage * 5;
       // Remove the tile padding. Now we're dealing with JUST tile sizes; no padding
       width = width - totalTilePadding;
       // Screen width, divided by number of tiles, gives us tile width (without padding)
@@ -126,12 +78,15 @@ $(document).ready(function () {
       // These classes may never be removed at this point, but this is to ensure
       // the consistency of the tiles.
       $(".tile--has-next, .tile--has-prev")
-        .removeClass("tile--remove-next-prev tile--remove-next-next");
+        .removeClass("tile--remove-next-prev tile--remove-next-next")
+        .find(".tile__hasnext, .tile__hasprev")
+        .remove();
 
       // Loop through each container and add [data-tiles], [data-max-pages] and
       // add [data-current-page]
       this._rows.containers.each(function (i, elem) {
-        let tiles = +$(this).find('.tile').length;
+        let $tiles = $(this).find('.tile');
+        let tiles = +$tiles.length;
         // Add a unique data attributes to the DOM
         $(elem).attr('data-tiles', tiles)
           .attr('data-max-pages', Math.floor(tiles / setup.settings.tilesPerPage))
@@ -143,19 +98,37 @@ $(document).ready(function () {
         // add the tile--has-next classes to the proper tiles. 
         // We do it in this loop so we dont need to run the same loop twice
         if (setup.settings.tilesPerPage < tiles) {
-          $(this).find('.tile:eq(' + (setup.settings.tilesPerPage) + ')')
-            .addClass('tile--has-next');
+         // $(this).find('.tile:eq(' + (setup.settings.tilesPerPage) + ')')
+
         }
 
-        // Set the "scale-left" tiles. These are tiles that, when scaling up,
-        // they push the tiles before it to the left (instead of pushing the 
-        // preceding tiles right.)
-        for (i = i; i <= tiles; i++) {
-          if (i % setup.settings.tilesPerPage == 0) {
-            $(this).find('.tile:eq(' + (i - 1) + ')')
-              .addClass('tile--scale-left')
+        var counter = 1;
+        $tiles.each(function(i, elem) {
+          if( counter > setup.settings.tilesPerPage ) {
+            counter = 1;
           }
-        }
+          
+          $(this).attr('counter', counter)
+
+          if( counter == 1 ) {
+            $(this).addClass("tile--scale-right");
+          } else if( counter == setup.settings.tilesPerPage ) {
+            $(this).addClass("tile--scale-left")
+              .next()
+              .addClass("tile--has-next")
+              .prepend("<div class='tile__hasnext'></div>");
+          } else {
+            $(this).addClass("tile--scale-center");
+          }
+
+          if(counter == setup.settings.tilesPerPage+1) {
+            alert(1);
+          }
+
+          counter++;
+        })
+
+        
       })
 
       // Chainable method
@@ -165,17 +138,7 @@ $(document).ready(function () {
 
   // Add a window resize method that re-inits setup.init() after mobile tiles are put together.
   setup.init({
-    tilesPerPage: 5,
-    useLocalStorage: true,
-    // $tile is the jQ selector for the currently active .tile. 
-    tileClickCallback:  function( $tile )  {
-      var tileId = $tile.attr('data-id');
-      console.log('Tile activated');
-
-    },
-    tileDeactivatedCallback: function () {
-      console.log('Tile deactivated');
-    },
+    tilesPerPage: 5
   });
 
 // A wrapper for the jquery ajax request. 
@@ -265,11 +228,62 @@ const formStarsFromRating = function(rating) {
 
 
   $(document)
+    // Scale a center tile.
+    .on("mouseover", ".row__inner:not(.row__inner--active) .tile:not(.tile--active, .tile--has-next, .tile--has-prev)", function() {
+
+      var _t = $(this);
+
+      var scaledWidth = _t.outerWidth(true) * 2;
+
+      if( _t.hasClass('tile--scale-center') ) {
+        // Center tile. 
+        var left = -(scaledWidth / 2 / 2);
+        var moveTilesRight = (left / -1);
+        var moveTilesLeft = left
+        var scaleTileLeft = (left/2)
+      } else if( _t.hasClass('tile--scale-right') ) {
+        // Left tile 
+        var left = -(scaledWidth / 2 / 2);
+        var moveTilesRight = scaledWidth / 2;
+        var moveTilesLeft = 0;
+        var scaleTileLeft = 0;
+      } else if( _t.hasClass('tile--scale-left') ) {
+        // Right tile.
+        var left = -(scaledWidth / 2 / 2);
+        var moveTilesRight = (scaledWidth / 2);
+        var moveTilesLeft = -(scaledWidth / 2);
+        var scaleTileLeft = left;
+      }
+
+      $(this).addClass('tile--hovered').css({
+        transform: "scale(2) translate3d("+scaleTileLeft+"px, 0, 0)",
+      })
+      _t.prevAll().addClass('tile--hovered-prev').css({
+        transform: "translate3d("+moveTilesLeft+"px, 0, 0)"
+      });
+      _t.nextAll().addClass('tile--hovered-next').css({
+        transform: "translate3d("+moveTilesRight+"px, 0, 0)"
+      });
+    })
+    .on("mouseout", ".tile--hovered", function() {
+      var _t = $(this);
+
+      $(this).removeClass('tile--hovered').css({
+        transform: "",
+      })
+      _t.prevAll().addClass('tile--hovered-prev').css({
+        transform: ""
+      });
+      _t.nextAll().addClass('tile--hovered-next').css({
+        transform: ""
+      });
+    })
     // Make a tile "active";
     // But only if it isn't already active, and isn't being covered be a "next" or "prev" block
     .on("click", ".tile:not(.tile--active, .tile--has-prev, .tile--has-next)", function (e) {
 
       var _t = $(this);
+      _t.trigger("mouseout");
       var _row = _t.closest(".row__inner");
       var _container = _t.closest(".row__container");
       var _preview = _container.find('.preview__container');
@@ -370,112 +384,36 @@ const formStarsFromRating = function(rating) {
     })
 
     // Scroll the row left 
-    .on("click", ".next, .tile--has-next", function (e) {
+    .on("click", ".tile--has-next, .tile--has-prev", function (e) {
+
+      var _t = $(this);
+
       // Row container selector 
-      var _container = $(this).closest('.row__container');
+      var _container = _t.closest('.row__container');
       // Inner row container 
       var _row = _container.find('.row__inner');
 
-      // Get the row data from the container 
-      var _data = {
-        tiles: +_container.attr('data-tiles'),
-        maxPages: +_container.attr('data-max-pages'),
-        currentPage: +_container.attr('data-current-page')
-      };
+      // How far to move the row. 
+      var move = (setup.settings.tileWidth * setup.settings.tilesPerPage + (setup.settings.tilesPerPage * 5));
+      // Which direction to move the row
+      var direction = _t.hasClass('tile--has-next') ? "+=" : "-=";
+      // Row data. 
+      var maxPages = +_container.attr('data-max-pages');
+      var currentPage = +_container.attr('data-current-page');
+          currentPage = currentPage + (_t.hasClass('tile--has-next') ? 1 : -1)
 
-      // If there are enough "pages" we can show the next page
-      if (_data.currentPage < _data.maxPages) {
-        // Check how many tiles are in the view port right now.. 
-        _row.animate({
-          // tileWidth x tilesPerPage; plus 10px per tile for padding
-          right: '+=' + (setup.settings.tileWidth * setup.settings.tilesPerPage + (setup.settings.tilesPerPage * 10)),
-        }, {
-          duration: 50,
-          easing: 'swing',
-          start: function removePrevAndNextOverlays() {
-            // Add the fact that this row was slid at least once,
-            _row.addClass('row__inner--slid');
-            // Don't let the tiles scale up until the sliding is finished.
-            // We'll add these back if we need them later.
-            _row.find('.tile--has-prev, .tile--has-next').removeClass('tile--has-prev tile--has-next');
-            console.log('Slide forward started');
-          },
-          done: function updateCurrentPage() {
-            // Update the [data-current-page] attribute 
-            _data.currentPage = _data.currentPage + 1
-            _container.attr('data-current-page', _data.currentPage);
-
-            // Set the new scroll arrows (called buttons)
-            setup.settings.setScrollButtons(_row, _data.currentPage);
-
-            console.log('Slide forward done');
-
-          },
-          complete: function () {
-            console.log('Slide forward complete')
-          }
-        })
-        console.log('Slide forward');
-      }
-      return e.preventDefault();
-    })
-
-    // Scroll the row right
-    .on("click", ".prev, .tile--has-prev", function (e) {
-      // Row container selector 
-      var _container = $(this).closest('.row__container');
-      // Inner row container 
-      var _row = _container.find('.row__inner');
-
-      // Get the row data from the container 
-      var _data = {
-        tiles: +_container.attr('data-tiles'),
-        maxPages: +_container.attr('data-max-pages'),
-        currentPage: +_container.attr('data-current-page')
-      };
-
-      // If there are enough "pages" we can show the next page
-      if (_data.currentPage > 0) {
-        // Check how many tiles are in the view port right now.. 
-        _row.animate({
-          right: '-=' + (setup.settings.tileWidth * setup.settings.tilesPerPage + (setup.settings.tilesPerPage * 10)) // tileWidth x tilesPerPage; plus 10px per tile for padding
-        }, {
-          duration: 50,
-          easing: 'swing',
-          start: function removePrevAndNextOverlays() {
-            _row.addClass('row__inner--slid');
-            // We'll add these back if we need them later.
-            _row.find('.tile--has-prev, .tile--has-next').removeClass('tile--has-prev tile--has-next');
-            console.log('Slide back started');
-          },
-          done: function updateCurrentPage() {
-            // Update the [data-current-page] attribute 
-            _data.currentPage = _data.currentPage - 1
-            _container.attr('data-current-page', _data.currentPage);
-
-            // Set the new scroll arrows (called buttons)
-            setup.settings.setScrollButtons(_row, _data.currentPage);
-
-            console.log('Slide back done');
-
-          },
-          complete: function () {
-            console.log('Slide back complete')
-          }
-        })
-        console.log('Slide back');
-      }
-      return e.preventDefault();
-    })
-
-    // When the user hovers over the last tile on a page 
-    .on("mouseover", ".tile.tile--scale-left:not(.tile--has-prev)", function () {
-      // Get the tile before it. 
-      $(this).closest('.row__inner:not(.row__inner--active)').addClass('row__inner--tile-last-hovered').find('.tile:first').css('margin-left', -$(this).outerWidth());
-    })
-    // When the user moves off the last tile, fix the styling 
-    .on("mouseout mouseleave", ".tile.tile--scale-left", function () {
-      $(this).closest('.row__inner').removeClass('row__inner--tile-last-hovered').find('.tile:first').css('margin-left', 0);
+      // Remove the arrows
+      _row.find(".tile__hasnext, .tile__hasprev").remove();
+      // Remove overlaying elements and the classes that allow sliding actions
+      _row.find('.tile--has-prev, .tile--has-next').removeClass('tile--has-prev tile--has-next');
+      // Update the [data-current-page] attribute 
+      _container.attr('data-current-page', currentPage);
+      // Animate the styling.
+      _row.css({
+        right: direction + move,
+      })
+      // Set the new scroll arrows (called buttons)
+      setup.settings.setScrollButtons(_row, currentPage);
     })
 
     // Close all tiles when there are any active tiles, and the `esc` key is pressed.
